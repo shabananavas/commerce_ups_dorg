1707ecca4df12f791deae729379e09b4776338d6
Shabana Navas <snavas@acromedia.com>
2018-11-16 18:25:35 +0530

    Add the delivery date to a shipping rate.

--- src/UPSRateRequest.php
+++ src/UPSRateRequest.php
@@ -5,10 +5,9 @@ namespace Drupal\commerce_ups;
 use const COMMERCE_UPS_LOGGER_CHANNEL;
 use Drupal\commerce_price\Price;
 use Drupal\commerce_shipping\Entity\ShipmentInterface;
-use Drupal\commerce_shipping\Plugin\Commerce\ShippingMethod\ShippingMethodInterface;
 use Drupal\commerce_shipping\ShippingRate;
 use Drupal\commerce_shipping\ShippingService;
-use Psr\Container\ContainerInterface;
+use Drupal\Core\Datetime\DrupalDateTime;
 use Ups\Rate;
 use Ups\Entity\RateInformation;
 
@@ -17,69 +16,43 @@ use Ups\Entity\RateInformation;
  *
  * @package Drupal\commerce_ups
  */
-class UPSRateRequest extends UPSRequest implements UPSRateRequestInterface {
+class UPSRateRequest extends UPSRequest {
 
   /**
-   * A shipping method configuration array.
+   * The commerce shipment entity.
+   *
+   * @var \Drupal\commerce_shipping\Entity\ShipmentInterface
+   */
+  protected $commerceShipment;
+
+  /**
+   * The configuration array from a CommerceShippingMethod.
    *
    * @var array
    */
   protected $configuration;
 
   /**
-   * The UPS Shipment object.
+   * Set the shipment for rate requests.
    *
-   * @var \Drupal\commerce_ups\UPSShipmentInterface
+   * @param \Drupal\commerce_shipping\Entity\ShipmentInterface $commerce_shipment
+   *   A Drupal Commerce shipment entity.
    */
-  protected $upsShipment;
-
-  /**
-   * UPSRateRequest constructor.
-   *
-   * @param \Drupal\commerce_ups\UPSShipmentInterface $ups_shipment
-   *   The UPS shipment object.
-   */
-  public function __construct(UPSShipmentInterface $ups_shipment) {
-    $this->upsShipment = $ups_shipment;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('commerce_ups.ups_shipment')
-    );
+  public function setShipment(ShipmentInterface $commerce_shipment) {
+    $this->commerceShipment = $commerce_shipment;
   }
 
   /**
    * Fetch rates from the UPS API.
-   *
-   * @param \Drupal\commerce_shipping\Entity\ShipmentInterface $commerce_shipment
-   *   The commerce shipment.
-   * @param \Drupal\commerce_shipping\Plugin\Commerce\ShippingMethod\ShippingMethodInterface $shipping_method
-   *   The shipping method.
-   *
-   * @throws \Exception
-   *   Exception when required properties are missing.
-   *
-   * @return array
-   *   An array of ShippingRate objects.
    */
-  public function getRates(ShipmentInterface $commerce_shipment, ShippingMethodInterface $shipping_method) {
-    $rates = [];
+  public function getRates() {
+    // Validate a commerce shipment has been provided.
+    if (empty($this->commerceShipment)) {
+      throw new \Exception('Shipment not provided');
+    }
 
-    try {
-      $auth = $this->getAuth();
-    }
-    catch (\Exception $exception) {
-      \Drupal::logger(COMMERCE_UPS_LOGGER_CHANNEL)->error(
-        dt(
-          'Unable to fetch authentication config for UPS. Please check your shipping method configuration.'
-        )
-      );
-      return [];
-    }
+    $rates = [];
+    $auth = $this->getAuth();
 
     $request = new Rate(
       $auth['access_key'],
@@ -89,7 +62,8 @@ class UPSRateRequest extends UPSRequest implements UPSRateRequestInterface {
     );
 
     try {
-      $shipment = $this->upsShipment->getShipment($commerce_shipment, $shipping_method);
+      $ups_shipment = new UPSShipment($this->commerceShipment);
+      $shipment = $ups_shipment->getShipment();
 
       // Enable negotiated rates, if enabled.
       if ($this->getRateType()) {
@@ -101,6 +75,14 @@ class UPSRateRequest extends UPSRequest implements UPSRateRequestInterface {
 
       // Shop Rates.
       $ups_rates = $request->shopRates($shipment);
+
+      // Create a time in transit object so we can get the transit time for this
+      // shipment.
+      $time_in_transit = new UPSTransitRequest(
+        $this->configuration,
+        $this->commerceShipment,
+        $shipment
+      );
     }
     catch (\Exception $ex) {
       \Drupal::logger(COMMERCE_UPS_LOGGER_CHANNEL)->error($ex->getMessage());
@@ -109,39 +91,43 @@ class UPSRateRequest extends UPSRequest implements UPSRateRequestInterface {
 
     if (!empty($ups_rates->RatedShipment)) {
       foreach ($ups_rates->RatedShipment as $ups_rate) {
-        $service_code = $ups_rate->Service->getCode();
+        // Let's add an underscore as prefix to the code we get from UPS as the
+        // keys for the UPS shipping services start with an underscore due to a
+        // bug in the Drupal Core annotation system.
+        $service_code = '_' . $ups_rate->Service->getCode();
 
         // Only add the rate if this service is enabled.
         if (!in_array($service_code, $this->configuration['services'])) {
           continue;
         }
 
-        // Use negotiated rates if they were returned.
-        if ($this->getRateType() && !empty($ups_rate->NegotiatedRates->NetSummaryCharges->GrandTotal->MonetaryValue)) {
-          $cost = $ups_rate->NegotiatedRates->NetSummaryCharges->GrandTotal->MonetaryValue;
-          $currency = $ups_rate->NegotiatedRates->NetSummaryCharges->GrandTotal->CurrencyCode;
-        }
-
-        // Otherwise, use the default rates.
-        else {
-          $cost = $ups_rate->TotalCharges->MonetaryValue;
-          $currency = $ups_rate->TotalCharges->CurrencyCode;
-        }
-
+        $cost = $ups_rate->TotalCharges->MonetaryValue;
+        $currency = $ups_rate->TotalCharges->CurrencyCode;
         $price = new Price((string) $cost, $currency);
         $service_name = $ups_rate->Service->getName();
+        $date = new DrupalDateTime();
+        $date->format('Y-m-d');
 
         $shipping_service = new ShippingService(
-          $service_code,
+          $service_name,
           $service_name
         );
-        $rates[] = new ShippingRate(
-          $service_code,
-          $shipping_service,
-          $price
-        );
+
+        if (isset($time_in_transit)) {
+          $times = $time_in_transit->getTransitTime();
+
+          foreach ($times->ServiceSummary as $serviceSummary) {
+            $rates[] = new ShippingRate(
+              $service_code,
+              $shipping_service,
+              $price,
+              $date::createFromFormat('Y-m-d', $serviceSummary->EstimatedArrival->getDate())
+            );
+          }
+        }
       }
     }
+
     return $rates;
   }
 
